a70d60e2f383f4e221f80587f94fbeeb54248ed1
Robert Matundu <matundukabamba@gmail.com>
2023-12-23 13:31:57 +0100

    je suis le premier commite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./component/header/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/component/header/Navbar.jsx
@@ -0,0 +1,12 @@
+
+function Navbar() {
+  return (
+    <>
+      <div className=" md:px-14 lg:px-14 sm:px-16 p-5 max-w-screen-2xl mx-auto">
+        
+      </div>
+    </>
+  );
+}
+
+export default Navbar
