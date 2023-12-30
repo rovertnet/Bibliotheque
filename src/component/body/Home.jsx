76cb0d303a432a4a8d6982b37b2a21d3743baad2
Robert Matundu <matundukabamba@gmail.com>
2023-12-30 14:38:28 +0100

    update of the card

--- src/component/body/Home.jsx
+++ src/component/body/Home.jsx
@@ -32,6 +32,7 @@ function Home() {
           <h1 className="text-center mt-10 md:mt-11 md:mb-3 mb-2 text-2xl md:text-3xl font-extrabold text-slate-900">
             Les plus consultés
           </h1>
+          <hr />
           <div className=" grid grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-5 md:my-4 my-2 md:py-10 py-9">
             <div className=" bg-white shadow-md dark:bg-slate-800 rounded-lg overflow-hidden hover:-translate-y-4 transition-all duration-300 cursor-pointer">
               <img
@@ -42,10 +43,10 @@ function Home() {
               <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
                 <div className=" flex justify-center items-center gap-3 px-5 md:px-7">
                   <h3 className="text- md:text-xl mb-3 md:mb-4 text-slate-900 font-bold">
-                    Informatique
+                    Robert
                   </h3>
                   <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-bold">
-                    Robert
+                    Algo
                   </h3>
                 </div>
                 <div className="flex justify-center items-center gap-3 px-10 md:text-xl md:px-7">
@@ -72,12 +73,12 @@ function Home() {
                 className=" w-full h-36 md:h-48 object-cover"
               />
               <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
-                <div className=" flex justify-center items-center gap-5">
+                <div className=" flex justify-center items-center space-x-2">
                   <h3 className="text- md:text-xl mb-3 md:mb-4 text-slate-900 font-bold">
-                    Informatique
+                    Robert
                   </h3>
                   <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-bold">
-                    Robert
+                    Anglais
                   </h3>
                 </div>
                 <div className="flex justify-center items-center gap-3 px-10 md:text-xl md:px-7">
@@ -104,12 +105,12 @@ function Home() {
                 className=" w-full h-36 md:h-48 object-cover"
               />
               <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
-                <div className=" flex justify-center items-center gap-5">
+                <div className=" flex justify-center items-center space-x-2">
                   <h3 className="text- md:text-xl mb-3 md:mb-4 text-slate-900 font-bold">
-                    Informatique
+                    Robert
                   </h3>
                   <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-bold">
-                    Robert
+                    Gest.pro
                   </h3>
                 </div>
                 <div className="flex justify-center items-center gap-3 px-10 md:text-xl md:px-7">
@@ -136,12 +137,12 @@ function Home() {
                 className=" w-full h-36 md:h-48 object-cover"
               />
               <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
-                <div className=" flex justify-center items-center gap-5">
+                <div className=" flex justify-center items-center space-x-2">
                   <h3 className="text- md:text-xl mb-3 md:mb-4 text-slate-900 font-bold">
-                    Informatique
+                    Robert
                   </h3>
                   <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-bold">
-                    Robert
+                    Economie
                   </h3>
                 </div>
                 <div className="flex justify-center items-center gap-3 px-10 md:text-xl md:px-7">
@@ -168,12 +169,12 @@ function Home() {
                 className=" w-full h-36 md:h-48 object-cover"
               />
               <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
-                <div className=" flex justify-center items-center gap-5">
+                <div className=" flex justify-center items-center space-x-2">
                   <h3 className="text- md:text-xl mb-3 md:mb-4 text-slate-900 font-bold">
-                    Informatique
+                    Robert
                   </h3>
                   <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-bold">
-                    Robert
+                    Psycho
                   </h3>
                 </div>
                 <div className="flex justify-center items-center gap-3 px-10 md:text-xl md:px-7">
@@ -200,12 +201,44 @@ function Home() {
                 className=" w-full h-36 md:h-48 object-cover"
               />
               <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
-                <div className=" flex justify-center items-center gap-5">
+                <div className=" flex justify-center items-center space-x-2">
                   <h3 className="text- md:text-xl mb-3 md:mb-4 text-slate-900 font-bold">
-                    Informatique
+                    Robert
                   </h3>
                   <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-bold">
+                    Dev.web
+                  </h3>
+                </div>
+                <div className="flex items-center justify-center gap-3 px-10 md:px-7">
+                  <button className=" text-lg font-medium">
+                    <FaShoppingCart />
+                  </button>
+                  <button className=" text-lg font-medium">
+                    <FaEye />
+                  </button>
+                  <button className=" text-lg font-medium">
+                    <BiBookmark />
+                  </button>
+                  <button className=" text-lg font-medium">
+                    <FaRegHeart />
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className=" bg-white shadow-md dark:bg-slate-800 rounded-lg overflow-hidden hover:-translate-y-4 transition-all duration-300 cursor-pointer">
+              <img
+                src="../../public/L9.jpg"
+                alt="couverture"
+                className=" w-full h-36 md:h-48 object-cover"
+              />
+              <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
+                <div className=" flex justify-center items-center space-x-2">
+                  <h3 className="text- md:text-xl mb-3 md:mb-4 text-slate-900 font-bold">
                     Robert
+                  </h3>
+                  <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-bold">
+                    Dev.web
                   </h3>
                 </div>
                 <div className="flex items-center justify-center gap-3 px-10 md:px-7">
@@ -232,12 +265,44 @@ function Home() {
                 className=" w-full h-36 md:h-48 object-cover"
               />
               <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
-                <div className=" flex justify-center items-center gap-5">
+                <div className=" flex justify-center items-center space-x-2">
                   <h3 className="text- md:text-xl mb-3 md:mb-4 text-slate-900 font-bold">
-                    Informatique
+                    Robert ||
                   </h3>
                   <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-bold">
-                    Robert
+                    Réseau
+                  </h3>
+                </div>
+                <div className="flex justify-center items-center gap-3 px-10 md:text-xl md:px-7">
+                  <button className=" text-lg font-medium">
+                    <FaShoppingCart />
+                  </button>
+                  <button className=" text-lg font-medium">
+                    <FaEye />
+                  </button>
+                  <button className=" text-lg font-medium">
+                    <BiBookmark />
+                  </button>
+                  <button className=" text-lg font-medium">
+                    <FaRegHeart />
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className=" bg-white shadow-md dark:bg-slate-800 rounded-lg overflow-hidden hover:-translate-y-4 transition-all duration-300 cursor-pointer">
+              <img
+                src="../../public/20.jpg"
+                alt="couverture"
+                className=" w-full h-36 md:h-48 object-cover"
+              />
+              <div className="text-gray-600 dark:text-gray-300 p-5 w-full">
+                <div className=" flex justify-center items-center space-x-2">
+                  <h3 className="text- md:text-xl mb-3 md:mb-4 text-slate-900 font-bold">
+                    Robert ||
+                  </h3>
+                  <h3 className="text-xl md:text-xl mb-3 md:mb-4 font-bold">
+                    Réseau
                   </h3>
                 </div>
                 <div className="flex justify-center items-center gap-3 px-10 md:text-xl md:px-7">
