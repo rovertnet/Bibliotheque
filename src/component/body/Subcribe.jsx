808c7166ad2d73ccf6b3eadebfa85b85253970b6
Robert Matundu <matundukabamba@gmail.com>
2023-12-30 15:22:42 +0100

    update of the banner

--- src/component/body/Subcribe.jsx
+++ src/component/body/Subcribe.jsx
@@ -1,16 +1,17 @@
+import { IoSend } from "react-icons/io5";
 
 function Subcribe() {
   return (
     <>
-      <div className=" md:px-14 px-4 max-w-screen-2xl mx-auto my-12">
-        <div className=" md:p-9 px-4 py-9 bg-gradient-to-r from-black via-white to-slate-800 ...">
-          <div className=" flex flex-col md:flex-row-reverse justify-between items-center gap-10">
+      <div className=" md:px-14 px-4 max-w-screen-2xl mx-auto my-10">
+        <div className=" md:p-9 px-4 py-9 rounded-md bg-gradient-to-r from-black via-slate-400 to-slate-800 ...">
+          <div className=" flex flex-col md:flex-row justify-between items-center gap-10">
             {/* image */}
             <div>
-              <h1 className=" text-2xl font-bold text-white md:text-3xl">
+              <h1 className=" text-3xl font-extrabold text-white md:text-4xl md:mb-3 mb-2">
                 Sign up for our newsletter
               </h1>
-              <p className=" text-slate-300 text-base font-medium md:text-lg">
+              <p className=" text-slate-200 text-xl font-medium md:text-xl">
                 Join our newsletter and get news in your inbox every week! We
                 hate spam too, so no worries about this.
               </p>
@@ -19,11 +20,18 @@ function Subcribe() {
             <div className=" md:w-full">
               {/* le formulaire */}
               <form action="">
-                <div className="">
+                <div className="flex justify-center items-center space-x-3">
                   <input
                     type="mail"
-                    className=" md:w-full w-full outline outline-0 focus:outline-0 "
+                    className=" md:w-full w-full outline outline-0 focus:outline-0 px-6 md:px-7 py-4 md:py-5 text-slate-700 rounded-md"
+                    placeholder="Votre adresse e-mail"
                   />
+                  <button
+                    type="submit"
+                    className="bg-black px-6 md:px-7 py-4 md:py-5 rounded-md hover:bg-slate-700"
+                  >
+                    <IoSend className="text-slate-200 text-2xl md:text-3xl font-semibold" />
+                  </button>
                 </div>
               </form>
             </div>
@@ -34,4 +42,4 @@ function Subcribe() {
   );
 }
 
-export default Subcribe
+export default Subcribe;
