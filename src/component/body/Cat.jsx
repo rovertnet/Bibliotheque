a9fc8a164615db991d824c94a99c1bf6edbe4d56
Robert Matundu <matundukabamba@gmail.com>
2024-01-01 11:02:16 +0100

    le premier commt de l'année

---
+++ src/component/body/Cat.jsx
@@ -0,0 +1,20 @@
+
+function Cat() {
+  return (
+    <>
+        <div className="md:px-14 px-12">
+            <div className="flex justify-center items-center gap-3">
+                <div className="">
+                    <img src="" alt="" className=" rounded-full" />
+                    <div>
+                        <h1>Informatique</h1>
+                        <span>12</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Cat
