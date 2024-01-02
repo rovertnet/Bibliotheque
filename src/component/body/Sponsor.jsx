81b25b3832389817f217982cd648c434946c6a7b
Robert Matundu <matundukabamba@gmail.com>
2024-01-02 11:15:49 +0100

    le premier commt de l'année

---
+++ src/component/body/Sponsor.jsx
@@ -0,0 +1,17 @@
+
+function Sponsor() {
+  return (
+    <>
+      <div className="md:px-14 px-12 md:py-12 py-10">
+        <h1 className="text-center md:mb-8 mb-5 text-2xl md:text-3xl font-extrabold text-slate-900">
+          Clients
+        </h1>
+        <div className="">
+            
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sponsor
