74951bdc68b257367653c90a53fcb4a10e13b1d2
Robert Matundu <matundukabamba@gmail.com>
2024-01-08 17:20:42 +0100

    je suis le premier commit de la semaine

--- src/component/body/Sponsor.jsx
+++ src/component/body/Sponsor.jsx
@@ -6,7 +6,9 @@ function Sponsor() {
         <h1 className="text-center dark:text-slate-300 md:mb-8 mb-5 text-2xl md:text-3xl font-extrabold text-slate-900">
           Clients
         </h1>
-        
+        <div className="">
+          
+        </div>
       </div>
     </>
   );
