4bc169340a59dd739bbd0c20b0a30ffc56a3a595
Robert Matundu <matundukabamba@gmail.com>
2024-01-11 16:09:14 +0100

    modif du comp

--- src/component/body/Livre.jsx
+++ src/component/body/Livre.jsx
@@ -4,7 +4,7 @@ function Livre() {
     <>
       <div className="md:px-16 px-10">
         <h1 className="text-center dark:text-slate-300 md:mb-8 mb-5 text-2xl md:text-3xl font-extrabold text-slate-900">
-          Clients
+          Les plus vendus
         </h1>
         <div className=""></div>
       </div>
