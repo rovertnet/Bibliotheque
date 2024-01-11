3354f46245ce9699aecf92688529b108dfc0c9ab
Robert Matundu <matundukabamba@gmail.com>
2024-01-11 16:06:49 +0100

    add the component

--- src/component/body/Livre.jsx
+++ src/component/body/Livre.jsx
@@ -2,13 +2,14 @@
 function Livre() {
   return (
     <>
-        <div className="md:px-16 px-10">
-            <div className="">
-                
-            </div>
-        </div>
+      <div className="md:px-16 px-10">
+        <h1 className="text-center dark:text-slate-300 md:mb-8 mb-5 text-2xl md:text-3xl font-extrabold text-slate-900">
+          Clients
+        </h1>
+        <div className=""></div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Livre
